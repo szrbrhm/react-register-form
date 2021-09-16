94d9f9c4dd1bfbb59758c61135c9c938ee113862
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-16 21:44:49 +0200

    add

--- src/pages/SimpleRegister.js
+++ src/pages/SimpleRegister.js
@@ -1,49 +1,48 @@
 import React from "react";
 import "../App.css";
 import { useFormik } from "formik";
-import * as Yup from 'yup';
-
+import * as Yup from "yup";
+const initialValues = {
+  name: "",
+  email: "",
+  userName: "",
+};
+const onSubmit = (values) => {
+  console.log("Values:", values);
+};
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .max(15, "Must be 15 characters or less")
+    .required("Required.Enter name"),
+  userName: Yup.string()
+    .max(15, "Must be 15 characters or less")
+    .required("Required.Enter user name"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Required.Enter email address"),
+});
 function SimpleRegister() {
   const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      userName: "",
-    },
-    onSubmit: (values) => {
-      console.log("Values:", values);
-    },
-//     validate: (values) => {
-//       let errors = {};
-//       if (!values.name) {
-//         errors.name = "Required.Please fill  area";
-//       }
-//       if (!values.email) {
-//         errors.email = "Required.Please fill area";
-//       } else if (
-//         !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-//       ) {
-//         errors.email = "Invalid email address";
-//       }
-//       if (!values.userName) {
-//         errors.userName = "Required.Please fill  area";
-//       }
-//       return errors;
-//     },
-
-
-    validationSchema:Yup.object({
-        name:Yup.string()
-        .max(15,"Must be 15 characters or less")
-        .required("Required.Enter name")
-    })
-});
-
-
-
-  // console.log("Formik values :",formik.values)
-  // console.log("Formik  :",formik)
-  console.log(`formik touch`, formik.touched)
+    initialValues,
+    onSubmit,
+    validationSchema,
+    // validate:values=>{
+    //     let errors={}
+    //     if (!values.name){
+    //         errors.name="Required.Please fill  area"
+    //     }
+    //     if (!values.email){
+    //         errors.email="Required.Please fill area"
+    //     }
+    //     else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    //        errors.email = 'Invalid email address';}
+    //     if (!values.userName){
+    //         errors.userName="Required.Please fill  area"
+    //     }
+    //     return errors
+    // }
+  });
+  console.log("formik:", formik);
   return (
     <div className="container">
       <h1>Simple Form</h1>
@@ -67,6 +66,7 @@ function SimpleRegister() {
           name="email"
           onChange={formik.handleChange}
           value={formik.values.email}
+          onBlur={formik.handleBlur}
         ></input>
         {formik.touched.email && formik.errors.email ? (
           <div className="errorStyle">{formik.errors.email} </div>
@@ -78,6 +78,7 @@ function SimpleRegister() {
           name="userName"
           onChange={formik.handleChange}
           value={formik.values.userName}
+          onBlur={formik.handleBlur}
         ></input>
         {formik.touched.userName && formik.errors.userName ? (
           <div className="errorStyle">{formik.errors.userName} </div>
