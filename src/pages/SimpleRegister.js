b25f895b819fb775cf3c8ea0054bf870d2f2c0b7
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-16 20:19:22 +0200

    create pages

---
+++ src/pages/SimpleRegister.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import '../App.css';
+import { useFormik } from "formik";
+
+function SimpleRegister() {
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            userName: ""
+        }
+    });
+    console.log("formik :>>", formik);
+
+
+
+    return (
+        <div className="container">
+            <h1>Simple Form</h1>
+
+            <form className="formStyle" onSubmit={formik.handleSubmit}>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name}></input>
+
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" name="email" onChange={formik.handleChange} value={formik.values.email}></input>
+
+                <label htmlFor="userName">User name</label>
+                <input type="text" id="userName" name="userName" onChange={formik.handleChange} value={formik.values.userName}></input>
+
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default SimpleRegister
